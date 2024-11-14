7c12c6b7a38d9e420ff7ed74d4d631cc1b9a99e8
user.name "Satyam Shinde <user.email "satyamshinde301@gmail.com>
2024-11-14 10:34:22 +0530

    a

---
+++ src/Components/InputData.js
@@ -0,0 +1,28 @@
+import React,{useState} from "react"
+function InputData() {
+    const [data,setData]=useState(null)
+    const [print,setPrint]=useState(false)
+
+
+function GetData(e) {
+    console.log(e.target.value);
+    setData(e.target.value)
+    setPrint(false)
+}
+
+    return(
+        <div>
+
+           {
+            print? <h1>{data}</h1>:null
+           }
+            <input type="text" onChange={GetData}/>
+
+
+            
+            <button onClick={()=>setPrint(true)}>Click me</button>
+        </div>
+    )
+}
+
+export default InputData 
